e15de72987e7cb645f0ff3029310f54abbb09633
C22UG154CSC023 <144765745+C22UG154CSC023@users.noreply.github.com>
2026-01-30 23:34:48 +0530

    Add files via upload

---
+++ Task6.py
@@ -0,0 +1,70 @@
+'''salary=float(input("Enter the salary:"))
+if salary<20000:
+    bonus=salary*0.10
+elif salary>=20000 or salary<=50000:
+    bonus=salary*0.15
+else:
+    bonus=salary*0.20
+print("bonus",bonus)'''
+
+'''name=input("Enter the user name:")
+password=int(input("Enter the password:"))
+user_name="admin"
+user_password=1234
+if name==user_name and password==user_password:
+    print("Login successfull")
+elif name==user_name and password!=user_password:
+    print("wrong password")
+else:
+    print("invalied user")'''
+    
+'''a=int(input("Enter the value:"))
+b=int(input("Enter the mark:"))
+if a>=75 and b>=40:
+    print("Eligibile for exam")
+else:
+    print("Not eligible for exam")'''
+    
+'''units=int(input("Enter the units:"))
+if units<=100:
+    bill=units*2
+elif units>100 and units<200:
+    bill=units*3
+else:
+     bill=units*5
+print("bill",bill)'''
+
+
+'''leave=int(input("Enter the leave days:"))
+if leave <=2:
+    print("Leave Approved")
+else:
+    print("Need manager Approvel")'''
+    
+'''login_time=float(input("Enter the time:"))
+if login_time<=9.30:
+    print("On Time")
+elif login_time>9.30 and login_time<=10.00:
+    print("Late")
+else:
+    print("Leave Reaquired")'''
+    
+'''role=input("Enter you role:")
+active_status=input("Enter your status:")
+a="Developer"
+b="Active"
+if role==a and active_status==b:
+    print("Allowed")
+else:
+    print("Not Allowed")'''
+
+
+'''Team=input("Mandatory or Optional:")
+a="Mandatory"
+b="Optional"
+if Team==a:
+    print("Project team")
+elif Team==b:
+    print("Support team")
+else:
+    print("Not required")'''
